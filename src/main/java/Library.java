d5afa2d4fba8f42e8b0e67618bd30053cf558de2
Kelsie Braidwood <kelsieiona@gmail.com>
2018-05-01 16:34:52 +0100

    Set up of files and method for getting books added and adding books added.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,7 +4,17 @@ public class Library {
     private ArrayList<Book> books;
 
     public Library(){
-        this.books = new ArrayList<Book>;
+        this.books = new ArrayList<>();
     }
+
+    public int getBookCount(){
+        return this.books.size();
+    }
+    public void addBook(Book book){
+        this.books.add(book);
+
+    }
+
+
 
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    Library library;
+    Book book;
+
+    @Before
+    public void before() {
+        library = new Library();
+        book = new Book();
+    }
+
+    @Test
+    public void canGetBookCount(){
+        assertEquals(0, library.getBookCount());
+    }
+    @Test
+    public void canAddBook(){
+        library.addBook(book);
+        assertEquals(1, library.getBookCount());
+    }
 }
